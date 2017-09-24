09d834b47575f78c44a6892741c08c26b8bf93c5
Madhusudana Reddy Singana <smr246@gmail.com>
2017-09-24 22:05:22 +0530

    Intial commit-

---
+++ ProjectA/src/main/java/com/me/pack/A.java
@@ -0,0 +1,20 @@
+package com.me.pack;
+
+public class A {
+	ARESP m1(AREQ areq) {
+		ARESP aresp=new ARESP();
+		
+		BRESP bresp=new BRESP();
+		
+		BREQ breq=new BREQ();
+		breq.setId(areq.getId());
+		breq.setName(areq.getName());
+		
+		B b=new B();
+		bresp=b.m2(breq);
+		
+		aresp.setDesc(bresp.getDesc());
+		
+		return aresp;
+	}
+}
---
+++ ProjectA/src/test/java/ATest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.me.pack.AREQ;
+
+public class ATest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		AREQ areq=new AREQ();
+		areq.setId("123");
+		areq.setName("madhu");
+		
+		
+	}
+
+}
